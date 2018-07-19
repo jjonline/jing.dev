aafd5d57d0771bcd4a7830190812204be2914e7d
Jea杨 <jjonline@jjonline.cn>
2018-07-19 14:44:57 +0800

    完善UE富文本编辑器后端

--- manage/public/plugin/ueditor/ueditor.config.js
+++ manage/public/plugin/ueditor/ueditor.config.js
@@ -42,14 +42,14 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "/upload/handle"
+        , serverUrl: "/manage/upload/handle"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars: [[
             'source', '|', 'undo', 'redo', '|',
             'bold', 'italic', 'underline', 'strikethrough', 'removeformat', 'pasteplain', '|', 'forecolor', 'backcolor', '|',
             'fontfamily', 'fontsize', '|',
-            'link', 'unlink', '|',
+            'link', 'unlink', '|', 'inserttable' ,'deletetable','|','attachment',
             'simpleupload'
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
