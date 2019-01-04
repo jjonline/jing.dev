790b21d48ada047bcae34f66a2041c6639afd5af
Jea杨 <jjonline@jjonline.cn>
2019-01-04 23:48:13 +0800

    com DB数据库结构

---
+++ source/customerdata.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本机MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost
+ Source Database       : customerdata
+
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : utf-8
+
+ Date: 01/04/2019 23:47:54 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
