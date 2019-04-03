1461e9d65ac18827056d42d2a171df5287c7d4e1
Jea杨 <jjonline@jjonline.cn>
2019-04-04 00:07:46 +0800

    封面图裁剪+fix

--- public/public/js/manage/page/setting.js
+++ public/public/js/manage/page/setting.js
@@ -9,10 +9,10 @@ $(function () {
         //上传销项裁剪插件
         utils.bindCutImageUploader("cut_upload",{
             // url:"4/3",// 可以单独设置后端接收上传的图片和额外参数的url，默认系统提供的附件资源管理器自动处理
-            rate:"4/3",// 设置裁剪图的比例
+            rate:config.cover_options.width + '/' + config.cover_options.height,// 设置裁剪图的比例
             title:"封面图裁剪上传", // 设置裁剪浮层的标题
-            width:400,//指定裁剪后图片宽度
-            height:300,//指定裁剪后图片高度
+            width:config.cover_options.width,//指定裁剪后图片宽度
+            height:config.cover_options.height,//指定裁剪后图片高度
             extraData:{'file_type':'image'},//额外塞入上传的post数据体重的filed-value对象数组
             success:function (data) {
                 if(data["error_code"] == 0)
