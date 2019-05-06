e448a8d17cff82b71fe8535af4e23f2564bd2ce1
Jea杨 <jjonline@jjonline.cn>
2019-05-06 10:17:09 +0800

    命令行文件Pascal

--- application/command.php
+++ application/command.php
@@ -10,10 +10,10 @@
 // +----------------------------------------------------------------------
 
 return [
-    'app\console\command\init',// 初始化项目配置
-    'app\console\command\tasks',// 基于swoole扩展的TCP异步服务器
-    'app\console\command\password',// 修改整站加密auth_key之后加密明文密码
-    'app\console\command\generate',// 自动生成后台列表类页面
+    'app\console\command\Init',// 初始化项目配置
+    'app\console\command\Tasks',// 基于swoole扩展的TCP异步服务器
+    'app\console\command\Password',// 修改整站加密auth_key之后加密明文密码
+    'app\console\command\Generate',// 自动生成后台列表类页面
     // Db migration
     'think\migration\command\migrate\Create',
     'think\migration\command\migrate\Run',
