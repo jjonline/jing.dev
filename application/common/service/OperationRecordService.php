1c6f921ddfa755e34a92510cf4220e51a260d73a
Jea杨 <jjonline@jjonline.cn>
2019-08-07 21:20:37 +0800

    添加环境标记env+完善操作日志

--- application/common/service/OperationRecordService.php
+++ application/common/service/OperationRecordService.php
@@ -36,56 +36,68 @@ class OperationRecordService
 
     /**
      * 通过操作流程名和业务ID获取操作记录
-     * @param string $operate_name 操作流程名称，一般是对应业务的数据表表名
-     * @param int    $business_id  具体的业务ID
-     * @param string $order        获取记录的排序规则，默认按时间降序排列DESC，可传ASC升序
+     * @param string $business_type 操作标识，一般使用操作对应的数据表名称即可
+     * @param int    $business_id   操作对应数据表的数据的id
+     * @param string $order         获取记录的排序规则，默认按时间降序排列DESC，可传ASC升序
      * @return array
      * @throws \think\exception\DbException
      */
-    public function getOperationRecordList($operate_name, $business_id, $order = 'DESC')
+    public function getOperationRecordList($business_type, $business_id, $order = 'DESC')
     {
-        return $this->OperationRecord->getOperationRecordList($operate_name, $business_id, $order);
+        return $this->OperationRecord->getOperationRecordList($business_type, $business_id, $order);
     }
 
     /**
-     * 新增具有操作流程的操作记录
-     * @param string $operation_name 操作流程的标识，一般使用具体的业务数据表的完整表名称
-     * @param int    $business_id    操作流程的业务ID，例如：采购单的业务流程记录时该ID即为采购单ID
-     * @param string $title          操作流程的操作简要说明标题
-     * @param string $desc           操作流程的操作说明
-     * @param int|null $operate_user_id 可选的操作者用户ID，默认不传则取当前登录用户信息
+     * 新增操作日志
+     * @param string  $operation_name 操作简要名称
+     * @param string  $operation_desc 操作较详细描述
+     * @param string  $business_type  操作标识，一般使用操作对应的数据表名称即可
+     * @param integer $business_id    操作对应数据表的数据的id
+     * @param mixed   $business_param 操作需要额外存储的数据，数据或标量值
+     * @param integer|null $operate_user_id 操作者用户id
      * @return bool
      * @throws Exception
      */
-    public function save($operation_name, $business_id, $title, $desc, $operate_user_id = null)
-    {
-        if (empty($operation_name) || empty($business_id) || empty($title) || empty($desc)) {
-            throw new Exception('参数错误：第1个参数操作流程标识，第2个参数业务ID，第3个参数业务标题，第4个参数业务描述');
+    public function save(
+        $operation_name,
+        $operation_desc,
+        $business_type,
+        $business_id,
+        $business_param = null,
+        $operate_user_id = null
+    ) {
+        if (empty($operation_name) || empty($business_type) || empty($business_id)) {
+            throw new Exception('参数错误：第1个参数操作简要描述，第2个参数操作较详细描述，第3个参数操作标识，第4个参数操作对应数据主键id，第5个参数需要额外存储的数据，第6个可选参数用户id');
         }
         if (!empty($operate_user_id)) {
             $user = $this->User->getFullUserInfoById($operate_user_id);
             if (empty($user)) {
-                throw new Exception('第5个参数指定的用户ID标识的用户不存在');
+                throw new Exception('第6个参数指定的用户ID标识的用户不存在');
             }
         } else {
             $user = Session::get('user_info');
         }
         if (mb_strlen($operation_name, 'utf-8') > 64) {
-            throw new Exception('第1个参数操作简要标题不得操作64字');
+            throw new Exception('第1个参数操作简要描述不得操作64字');
         }
-        if (mb_strlen($desc, 'utf-8') > 512) {
-            throw new Exception('第4个参数操作流程的说明不得大于512字');
+        if (mb_strlen($operation_desc, 'utf-8') > 512) {
+            throw new Exception('第2个参数操作较详细描述不得大于512字');
+        }
+        if (strlen($business_type) > 64) {
+            throw new Exception('第3个参数操作标识不得大于64字符');
+        }
+        if (!is_numeric($business_id)) {
+            throw new Exception('第4个参数操作对应数据主键id必须是整数');
         }
         // 构造记录数组
         $operate                       = [];
-        $operate['operation_name']     = $operation_name;
+        $operate['operation_name']     = trim($operation_name);
+        $operate['operation_desc']     = trim($operation_desc);
+        $operate['business_type']      = $business_type;
         $operate['business_id']        = $business_id;
-        $operate['title']              = trim($title);
-        $operate['desc']               = trim($desc);
-        $operate['creator']            = $user['id'];
+        $operate['business_param']     = $business_param;
+        $operate['creator_id']         = $user['id'];
         $operate['creator_name']       = $user['real_name'];
-        $operate['creator_dept_id']    = $user['dept_id'];
-        $operate['creator_dept_name']  = $user['dept_name'];
 
         $ret = $this->OperationRecord->db()->insert($operate);
 
